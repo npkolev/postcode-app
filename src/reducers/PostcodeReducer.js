3129fbdec52e049721de3f5a167d8fac3644b7f3
Nick Kolev <kolev79@gmail.com>
2019-06-27 21:01:27 +0100

    Removed console.log()

--- src/reducers/PostcodeReducer.js
+++ src/reducers/PostcodeReducer.js
@@ -4,17 +4,14 @@ import * as types from '../Types'
 export default function createReducer(state = initialState, action) {
     switch(action.type) {
         case types.SEARCH_POSTCODE:
-            console.log(action.payload)
             return Object.assign({}, state, {
                 data: action.payload
             });
         case types.SEARCH_NEAREST_POSTCODES:
-            console.log(action.payload)
             return Object.assign({}, state, {
                 nearest: action.payload
             });
         case types.AUTOCOMPLETE_POSTCODE:
-            console.log(action.payload)
             return Object.assign({}, state, {
                 autocomplete: action.payload
             });
